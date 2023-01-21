2cb6d3fe53b006dfe2368bf656904456d57928df
suebin <suuebin@gmail.com>
2023-01-21 23:25:15 +0900

    [백트래킹] N-Queen

---
+++ suebin/boj/backtracking/N-Queen.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+        
+public class boj {
+	static int N;
+	static int[] queenPlace;
+	static int count = 0;
+    
+	public static void main(String[] args) throws IOException {
+    
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	N = Integer.parseInt(br.readLine());
+    	queenPlace = new int[N];
+    	findNQueen(0);	
+    	System.out.println(count);
+    }
+    
+	
+    public static void findNQueen(int depth) {
+    	if (depth == N) {
+    		count++;
+    		return;
+    	}
+    	
+    	for (int i = 0; i < N; i++) {
+    		queenPlace[depth] = i;
+    		
+    		if (getPossibillity(depth)) {
+        		findNQueen(depth+1);
+        	}
+    	}
+   
+    }
+    
+    public static boolean getPossibillity(int column) {
+    	for (int i = 0; i < column; i++) {
+    		if (queenPlace[column] == queenPlace[i])
+    			return false;
+    		
+    		if (Math.abs(column - i) == Math.abs(queenPlace[column] - queenPlace[i]))
+    			return false;
+    	}
+    	
+    	return true;
+    }
+}
